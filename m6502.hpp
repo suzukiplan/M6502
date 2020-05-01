7164ef117a18bc004691fb09ce1d91bfe361fbde
suzukiplan <suzukiplan.ys@gmail.com>
2020-05-01 10:39:56 +0900

    implement branch

--- m6502.hpp
+++ m6502.hpp
@@ -188,24 +188,48 @@ private:
         n ? R.p |= 0b10000000 : R.p &= 0b01111111;
     }
 
+    inline bool getStatusN() {
+        return R.p & 0b10000000 ? true : false;
+    }
+
     inline void updateStatusV(bool v) {
         v ? R.p |= 0b01000000 : R.p &= 0b10111111;
+    }
+
+    inline bool getStatusV() {
+        return R.p & 0b01000000 ? true : false;
     }
 
     inline void updateStatusB(bool b) {
         b ? R.p |= 0b00010000 : R.p &= 0b11101111;
     }
 
+    inline bool getStatusB() {
+        return R.p & 0b00010000 ? true : false;
+    }
+
     inline void updateStatusD(bool d) {
         d ? R.p |= 0b00001000 : R.p &= 0b11110111;
     }
 
-    inline void updateStatusD(bool i) {
+    inline bool getStatusD() {
+        return R.p & 0b00001000 ? true : false;
+    }
+
+    inline void updateStatusI(bool i) {
         i ? R.p |= 0b00000100 : R.p &= 0b11111011;
+    }
+
+    inline bool getStatusI() {
+        return R.p & 0b00000100 ? true : false;
     }
 
     inline void updateStatusZ(bool z) {
         z ? R.p |= 0b00000010 : R.p &= 0b11111101;
+    }
+
+    inline bool getStatusZ() {
+        return R.p & 0b00000010 ? true : false;
     }
 
     inline void updateStatusC(bool c) {
@@ -224,6 +248,36 @@ private:
         cpu->updateStatusN(cpu->R.a & 0x80);
         cpu->updateStatusZ(cpu->R.a == 0);
         cpu->updateStatusC(a & 0xFF00 ? true : false);
+    }
+
+    inline void and(unsigned char value) {
+        cpu->R.a &= value;
+        cpu->updateStatusN(cpu->R.a & 0x80);
+        cpu->updateStatusZ(cpu->R.a == 0);
+    }
+
+    inline unsigned char asl(unsigned char value) {
+        int work = value;
+        work <<= 1;
+        unsigned char result = work & 0xFF;
+        updateStatusN(R.a & 0x80);
+        updateStatusZ(R.a == 0);
+        updateStatusC(a & 0xFF00 ? true : false);
+        clockConsume();
+        return result;
+    }
+
+    // use 1, 2 or 3 cycles
+    inline void branch(bool isBranch) {
+        unsigned char rel = fetch();
+        if (!isBranch) return; // not branch
+        unsigned addr = R.pc - 2;
+        if (0xFF < (addr & 0xFF) + rel) {
+            consumeClock(); // consume a penalty cycle
+        }
+        addr += rel;
+        R.pc = addr;
+        consumeClock();
     }
 
     // code=$69, len=2, cycle=2
@@ -273,15 +327,51 @@ private:
         cpu->adc(cpu->readIndirectY(&addr));
     }
 
-    inline unsigned char asl(unsigned char value) {
-        int work = value;
-        work <<= 1;
-        unsigned char result = work & 0xFF;
-        updateStatusN(R.a & 0x80);
-        updateStatusZ(R.a == 0);
-        updateStatusC(a & 0xFF00 ? true : false);
-        clockConsume();
-        return result;
+    // code=$29, len=2, cycle=2
+    static inline void and_imm(M6502* cpu) {
+        cpu->and(cpu->fetch());
+    }
+
+    // code=$25, len=2, cycle=3
+    static inline void and_zpg(M6502* cpu) {
+        unsigned char addr;
+        cpu->and(cpu->readZeroPage(&addr));
+    }
+
+    // code=$35, len=2, cycle=4
+    static inline void and_zpg_x(M6502* cpu) {
+        unsigned char addr;
+        cpu->and(cpu->readZeroPageX(&addr));
+    }
+
+    // code=$2D, len=3, cycle=4
+    static inline void and_abs(M6502* cpu) {
+        unsigned short addr;
+        cpu->and(cpu->readAbsolute(&addr));
+    }
+
+    // code=$3D, len=3, cycle=4 or 5
+    static inline void and_abs_x(M6502* cpu) {
+        unsigned short addr;
+        cpu->and(cpu->readAbsoluteX(&addr));
+    }
+
+    // code=$39, len=3, cycle=4 or 5
+    static inline void and_abs_y(M6502* cpu) {
+        unsigned short addr;
+        cpu->and(cpu->readAbsoluteY(&addr));
+    }
+
+    // code=$21, len=2, cycle=6
+    static inline void and_x_ind(M6502* cpu) {
+        unsigned short addr;
+        cpu->and(cpu->readIndirectX(&addr));
+    }
+
+    // code=$31, len=2, cycle=5 or 6
+    static inline void and_ind_y(M6502* cpu) {
+        unsigned short addr;
+        cpu->and(cpu->readIndirectY(&addr));
     }
 
     // code=$0A, len=1, cycle=2
@@ -317,8 +407,44 @@ private:
         writeMemory(addr, m);
     }
 
+    // code=$30, len=2, cycle=3, 4 or 5
+    static inline void bmi_rel(M6502* cpu) {
+        cpu->branch(cpu->getStatusN());
+    }
+
+    // code=$10, len=2, cycle=3, 4 or 5
     static inline void bpl_rel(M6502* cpu) {
-        // TODO
+        cpu->branch(!cpu->getStatusN());
+    }
+
+    // code=$70, len=2, cycle=3, 4 or 5
+    static inline void bvs_rel(M6502* cpu) {
+        cpu->branch(cpu->getStatusV());
+    }
+
+    // code=$50, len=2, cycle=3, 4 or 5
+    static inline void bvc_rel(M6502* cpu) {
+        cpu->branch(!cpu->getStatusV());
+    }
+
+    // code=$F0, len=2, cycle=3, 4 or 5
+    static inline void beq_rel(M6502* cpu) {
+        cpu->branch(cpu->getStatusZ());
+    }
+
+    // code=$D0, len=2, cycle=3, 4 or 5
+    static inline void bne_rel(M6502* cpu) {
+        cpu->branch(!cpu->getStatusZ());
+    }
+
+    // code=$B0, len=2, cycle=3, 4 or 5
+    static inline void bcs_rel(M6502* cpu) {
+        cpu->branch(cpu->getStatusC());
+    }
+
+    // code=$90, len=2, cycle=3, 4 or 5
+    static inline void bcc_rel(M6502* cpu) {
+        cpu->branch(!cpu->getStatusC());
     }
 
     static inline void brk_impl(M6502* cpu) {
@@ -401,29 +527,51 @@ private:
     }
 
     void setupOperands(){
+        memset(operands, 0, sizeof(operands));
         operands[0x00] = brk_impl;
-        operands[0x01] = ora_x_ind;
-        operands[0x02] = NULL;
-        operands[0x03] = NULL;
-        operands[0x04] = NULL;
-        operands[0x05] = ora_zpg;
-        operands[0x06] = asl_zpg;
-        operands[0x07] = NULL;
         operands[0x08] = php_impl;
-        operands[0x09] = ora_imm;
+
+        operands[0x69] = adc_imm;
+        operands[0x65] = adc_zpg;
+        operands[0x75] = adc_zpg_x;
+        operands[0x6D] = adc_abs;
+        operands[0x7D] = adc_abs_x;
+        operands[0x79] = adc_abs_y;
+        operands[0x61] = adc_x_ind;
+        operands[0x71] = adc_ind_y;
+
+        operands[0x29] = and_imm;
+        operands[0x25] = and_zpg;
+        operands[0x35] = and_zpg_x;
+        operands[0x2D] = and_abs;
+        operands[0x3D] = and_abs_x;
+        operands[0x39] = and_abs_y;
+        operands[0x21] = and_x_ind;
+        operands[0x31] = and_ind_y;
+
         operands[0x0A] = asl_a;
-        operands[0x0B] = NULL;
-        operands[0x0C] = NULL;
-        operands[0x0D] = ora_abs;
-        operands[0x0E] = asl_abs;
-        operands[0x0F] = NULL;
-        operands[0x10] = bpl_rel;
-        operands[0x11] = ora_ind_y;
-        operands[0x15] = ora_zpg_x;
+        operands[0x06] = asl_zpg;
         operands[0x16] = asl_zpg_x;
-        operands[0x19] = ora_abs_y;
-        operands[0x1D] = ora_abs_x;
+        operands[0x0E] = asl_abs;
         operands[0x1E] = asl_abs_x;
+
+        operands[0x30] = bmi_rel;
+        operands[0x10] = bpl_rel;
+        operands[0x70] = bvs_rel;
+        operands[0x50] = bvc_rel;
+        operands[0xF0] = beq_rel;
+        operands[0xD0] = bne_rel;
+        operands[0xB0] = bcs_rel;
+        operands[0x90] = bcc_rel;
+
+        operands[0x09] = ora_imm;
+        operands[0x05] = ora_zpg;
+        operands[0x15] = ora_zpg_x;
+        operands[0x0D] = ora_abs;
+        operands[0x1D] = ora_abs_x;
+        operands[0x19] = ora_abs_y;
+        operands[0x01] = ora_x_ind;
+        operands[0x11] = ora_ind_y;
     }
 };
 
